2a9db4b41e1743e7a7de8e765d6328fd1379b093
jiangyanglinlan <1815212328@qq.com>
2018-05-25 20:01:26 +0800

    基于令牌的认证

--- app/models.py
+++ app/models.py
@@ -249,6 +249,19 @@ class User(UserMixin, db.Model):
                 db.session.add(user)
                 db.session.commit()
 
+    def generate_auth_token(self, expiration):
+        s = Serializer(current_app.config['SECRET_KEY'], expires_in=expiration)
+        return s.dumps({'id': self.id})
+
+    @staticmethod
+    def verify_auth_token(token):
+        s = Serializer(current_app.config['SECRET_KEY'])
+        try:
+            data = s.loads(token)
+        except:
+            return None
+        return User.query.get(data['id'])
+
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
